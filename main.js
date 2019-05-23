15b2f8a84399bf6fa9327cc9a27493a171c8cf45
Charlotte <charlotterushen@hotmail.com>
2019-05-23 17:13:23 +0100

    Adding nav links and animations

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.navigation-list');
+const navLinks = document.querySelectorAll('.nav-links');
 
 hamburger.addEventListener('click', (e) => {
   hamburger.classList.toggle('active');
+  navList.classList.toggle('active');
+  for(let i = 0; i < navLinks.length; i++) {
+    navLinks[i].classList.toggle('active');
+  }
 });
