6a3bf336d5704bf898b54ea9850f761172010d3d
Charlotte <charlotterushen@hotmail.com>
2019-05-24 15:40:26 +0100

    Fixing overlay

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const overlay = document.querySelector('#overlay');
 
 hamburger.addEventListener('click', (e) => {
   overlay.classList.toggle('nav-is-active');
+  if(overlay.classList.contains('nav-is-active')) {
+    overlay.style.zIndex = '1';
+  } else {
+    overlay.style.zIndex = '-1';
+  }
   hamburger.classList.toggle('active');
   navList.classList.toggle('active');
   for(let i = 0; i < navLinks.length; i++) {
