12b374854761cf3a781170ee18dd7d6df41575b1
Charlotte <charlotterushen@hotmail.com>
2019-05-24 15:18:20 +0100

    Adding overlay to page when nav is active

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.navigation-list');
 const navLinks = document.querySelectorAll('.nav-links');
+const overlay = document.querySelector('#overlay');
 
 hamburger.addEventListener('click', (e) => {
+  overlay.classList.toggle('nav-is-active');
   hamburger.classList.toggle('active');
   navList.classList.toggle('active');
   for(let i = 0; i < navLinks.length; i++) {
     navLinks[i].classList.toggle('active');
   }
+
 });
