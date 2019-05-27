d98ce1efec068beb03e1ff3afac0f88dbda5cbd1
Charlotte Rushen <charlotterushen@hotmail.com>
2019-05-27 12:02:43 +0100

    Adding sections, and click event on navLinks

--- main.js
+++ main.js
@@ -3,16 +3,27 @@ const navList = document.querySelector('.navigation-list');
 const navLinks = document.querySelectorAll('.nav-links');
 const overlay = document.querySelector('#overlay');
 
-hamburger.addEventListener('click', (e) => {
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navList.classList.toggle('active');
+  for(let i = 0; i < navLinks.length; i++) {
+    navLinks[i].classList.toggle('active');
+  }
   overlay.classList.toggle('nav-is-active');
   if(overlay.classList.contains('nav-is-active')) {
     overlay.style.zIndex = '1';
   } else {
     overlay.style.zIndex = '-1';
   }
-  hamburger.classList.toggle('active');
-  navList.classList.toggle('active');
-  for(let i = 0; i < navLinks.length; i++) {
-    navLinks[i].classList.toggle('active');
+});
+
+navList.addEventListener('click', (event) => {
+  if(event.target.tagName === 'A') {
+    hamburger.classList.remove('active');
+    navList.classList.remove('active');
+    overlay.classList.remove('nav-is-active');
+    for(let i = 0; i < navLinks.length; i++) {
+      navLinks[i].classList.remove('active');
+    }
   }
 });
